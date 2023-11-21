ea85ff911c32de79dee6c503248d68ed29825a24
Yoofi Quansah <ybquansah@gmail.com>
2023-11-21 09:25:42 -0600

    feat: add parameterization for tests

--- build/main.go
+++ build/main.go
@@ -2,17 +2,60 @@ package main
 
 import (
 	"context"
+	"flag"
+	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"dagger.io/dagger"
 )
 
+var (
+	languages    string
+	languageToFn = map[string]integrationTestFn{
+		"python": pythonTests,
+	}
+)
+
+type integrationTestFn func(context.Context, *dagger.Client, *dagger.Container, *dagger.File, *dagger.Directory) error
+
+func init() {
+	flag.StringVar(&languages, "languages", "", "comma separated list of which language(s) to run integration tests for")
+}
+
 func main() {
+	flag.Parse()
+
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
+	var languagesTestsMap = map[string]integrationTestFn{
+		"python": pythonTests,
+	}
+
+	if languages != "" {
+		l := strings.Split(languages, ",")
+		tlm := make(map[string]integrationTestFn, len(l))
+		for _, language := range l {
+			if testFn, ok := languageToFn[language]; !ok {
+				return fmt.Errorf("language %s is not supported", language)
+			} else {
+				tlm[language] = testFn
+			}
+		}
+
+		languagesTestsMap = tlm
+	}
+
 	ctx := context.Background()
+
 	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	defer client.Close()
 
@@ -20,11 +63,18 @@ func main() {
 
 	flipt, dynamicLibrary := getTestDependencies(ctx, client, dir)
 
-	if err := pythonTests(context.Background(), client, flipt, dynamicLibrary, dir); err != nil {
-		log.Fatal(err)
+	for _, testFn := range languagesTestsMap {
+		err := testFn(ctx, client, flipt, dynamicLibrary, dir)
+		if err != nil {
+			return err
+		}
 	}
+
+	return nil
 }
 
+// getTestDependencies builds the dynamic library for the Rust core, and the Flipt container for the client libraries to run
+// their tests against.
 func getTestDependencies(ctx context.Context, client *dagger.Client, hostDirectory *dagger.Directory) (*dagger.Container, *dagger.File) {
 	// Dynamic Library
 	rust := client.Container().From("rust:1.73.0-buster").
@@ -34,7 +84,7 @@ func getTestDependencies(ctx context.Context, client *dagger.Client, hostDirecto
 		WithExec([]string{"cargo", "build", "--release"})
 
 	// Flipt
-	flipt := client.Container().From("flipt/flipt:latest").
+	flipt := client.Container().From("flipt/flipt:nightly").
 		WithUser("root").
 		WithExec([]string{"mkdir", "-p", "/var/data/flipt"}).
 		WithDirectory("/var/data/flipt", hostDirectory.Directory("build/fixtures/testdata")).
@@ -47,6 +97,7 @@ func getTestDependencies(ctx context.Context, client *dagger.Client, hostDirecto
 	return flipt, rust.File("/app/target/release/libfliptengine.so")
 }
 
+// pythonTests runs the Poetry test suite against a container running Flipt through the dynamic library.
 func pythonTests(ctx context.Context, client *dagger.Client, flipt *dagger.Container, dynamicLibrary *dagger.File, hostDirectory *dagger.Directory) error {
 	// Poetry tests
 	_, err := client.Container().From("python:3.11-buster").
