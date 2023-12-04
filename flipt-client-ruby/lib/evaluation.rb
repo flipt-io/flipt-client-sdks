b1c3a80ad7d6e97eadedb80df9854fea8b0d5ab3
Mark Phelps <209477+markphelps@users.noreply.github.com>
2023-12-04 15:15:41 -0500

    chore: rename underlying calls

--- flipt-client-ruby/lib/evaluation.rb
+++ flipt-client-ruby/lib/evaluation.rb
@@ -30,10 +30,10 @@ module Flipt
     attach_function :initialize_engine, %i[pointer string], :pointer
     # void destroy_engine(void *engine_ptr);
     attach_function :destroy_engine, [:pointer], :void
-    # const char *variant(void *engine_ptr, const char *evaluation_request);
-    attach_function :variant, %i[pointer string], :string
-    # const char *boolean(void *engine_ptr, const char *evaluation_request);
-    attach_function :boolean, %i[pointer string], :string
+    # const char *evaluate_variant(void *engine_ptr, const char *evaluation_request);
+    attach_function :evaluate_variant, %i[pointer string], :string
+    # const char *evaluate_boolean(void *engine_ptr, const char *evaluation_request);
+    attach_function :evaluate_boolean, %i[pointer string], :string
 
     def initialize(namespace = 'default', opts = {})
       @namespace = namespace
@@ -54,14 +54,14 @@ module Flipt
     def evaluate_variant(evaluation_request = {})
       evaluation_request[:namespace_key] = @namespace
       validate_evaluation_request(evaluation_request)
-      resp = self.class.variant(@engine, evaluation_request.to_json)
+      resp = self.class.evaluate_variant(@engine, evaluation_request.to_json)
       JSON.parse(resp)
     end
 
     def evaluate_boolean(evaluation_request = {})
       evaluation_request[:namespace_key] = @namespace
       validate_evaluation_request(evaluation_request)
-      resp = self.class.boolean(@engine, evaluation_request.to_json)
+      resp = self.class.evaluate_boolean(@engine, evaluation_request.to_json)
       JSON.parse(resp)
     end
 
