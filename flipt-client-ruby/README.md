472ca6caf1529a7a02fbdf21cf5002765886a164
Mark Phelps <209477+markphelps@users.noreply.github.com>
2023-12-08 15:14:41 -0500

    chore: add loadtest script for ruby

--- flipt-client-ruby/README.md
+++ flipt-client-ruby/README.md
@@ -69,3 +69,22 @@ resp = client.evaluate_variant({ flag_key: 'buzz', entity_id: 'someentity', cont
 
 puts resp
 ```
+
+## Load Test
+
+1. To run the load test, you'll need to have Flipt running locally. You can do this by running the following command from the root of the repository:
+
+    ```bash
+    docker run -d \
+        -p 8080:8080 \
+        -p 9000:9000 \
+        docker.flipt.io/flipt/flipt:latest
+    ```
+
+2. You'll also need to have the `flipt_client` gem installed locally. See [Installation](#installation) above.
+3. In the Flipt UI (<http://localhost:8080>) you'll also need to create a new boolean flag with the key `my-feature` in the default namespace.
+4. You can then run the load test by running the following command from this folder:
+
+    ```bash
+    bundle exec ruby load_test.rb
+    ```
---
+++ flipt-client-ruby/loadtest.rb
@@ -0,0 +1,31 @@
+require "../flipt-client-ruby/lib/evaluation"
+
+# note: this script assumes you have built the flipt-client Ruby gem locally via the instructions in the README
+# and that the flag "my_feature" exists in the default namespace
+# and that the flag is a boolean flag
+
+FLIPT_URL = ENV.fetch("FLIPT_URL", "http://localhost:8080")
+
+NUM_CLIENTS = 100
+NUM_REQUESTS_PER_CLIENT = 1000
+
+# create 100 instances of the client
+clients = []
+
+NUM_CLIENTS.times do
+    clients << Flipt::EvaluationClient.new("default", {url: FLIPT_URL})
+end
+
+# capture start time
+start_time = Time.now
+
+# each client will make 1000 requests
+clients.each do |client|
+    NUM_REQUESTS_PER_CLIENT.times do
+        entity_id = rand(1000000).to_s
+        resp = client.evaluate_boolean({flag_key: "my-feature", entity_id: entity_id})
+    end
+end
+
+puts "done: #{clients.length} clients made #{clients.length * NUM_REQUESTS_PER_CLIENT} requests in total"
+puts "total time: #{Time.now - start_time} seconds"
