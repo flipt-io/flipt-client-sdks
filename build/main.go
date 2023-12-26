c51ef72359ef53ab7dde362cf824ccef996c4aba
Mark Phelps <209477+markphelps@users.noreply.github.com>
2023-12-26 10:47:31 -0500

    chore: fix rubygem push

--- build/main.go
+++ build/main.go
@@ -203,7 +203,6 @@ func rubyBuild(ctx context.Context, client *dagger.Client, hostDirectory *dagger
 	container := client.Container().From("ruby:3.1-bookworm").
 		WithWorkdir("/app").
 		WithDirectory("/app", hostDirectory.Directory("flipt-client-ruby")).
-		WithExec([]string{"sh", "-c", "export GEM_VERSION=$(cat /app/lib/flipt_client/version.rb | grep VERSION | cut -d' ' -f 5 | tr -d ')"}).
 		WithDirectory("/app/lib/ext", hostDirectory.Directory("tmp")).
 		WithExec([]string{"bundle", "install"}).
 		WithExec([]string{"bundle", "exec", "rake", "build"})
@@ -223,7 +222,7 @@ func rubyBuild(ctx context.Context, client *dagger.Client, hostDirectory *dagger
 
 	_, err = container.
 		WithSecretVariable("GEM_HOST_API_KEY", gemHostAPIKeySecret).
-		WithExec([]string{"gem", "push", "/app/pkg/flipt_client-${GEM_VERSION}.gem"}).
+		WithExec([]string{"sh", "-c", "gem push pkg/flipt_client-*.gem"}).
 		Sync(ctx)
 
 	return err
