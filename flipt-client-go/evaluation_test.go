43419bb630dbf01493dd6b3f09aeac57f04d2440
Mark Phelps <209477+markphelps@users.noreply.github.com>
2023-12-26 15:32:41 -0500

    fix: fix go mod path

--- flipt-client-go/evaluation_test.go
+++ flipt-client-go/evaluation_test.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	evaluation "go.flipt.io/flipt/flipt-client"
+	evaluation "go.flipt.io/flipt-client"
 )
 
 var fliptUrl string
--- flipt-client-go/go.mod
+++ flipt-client-go/go.mod
@@ -1,4 +1,4 @@
-module go.flipt.io/flipt/flipt-client
+module go.flipt.io/flipt-client
 
 go 1.21.3
 
