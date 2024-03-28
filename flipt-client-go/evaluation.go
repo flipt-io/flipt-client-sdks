bc89a76e5e5a26289bfc01f5ae686c60f86f4760
Roman Dmytrenko <rdmytrenko@gmail.com>
2024-03-28 21:31:07 +0200

    Apply suggestions from code review
    Co-authored-by: Mark Phelps <209477+markphelps@users.noreply.github.com>

--- flipt-client-go/evaluation.go
+++ flipt-client-go/evaluation.go
@@ -3,9 +3,8 @@ package evaluation
 /*
 #cgo CFLAGS: -I./ext
 #cgo darwin,arm64 LDFLAGS: -L${SRCDIR}/ext/darwin_arm64 -lfliptengine  -Wl,-rpath,${SRCDIR}/ext/darwin_arm64
-#cgo darwin,amd64 LDFLAGS: -L${SRCDIR}/ext/darwin_x86_64 -lfliptengine  -Wl,-rpath,${SRCDIR}/ext/darwin_x86_64
 #cgo linux,arm64 LDFLAGS: -L${SRCDIR}/ext/linux_arm64 -lfliptengine  -Wl,-rpath,${SRCDIR}/ext/linux_arm64
-#cgo linux,amd64 LDFLAGS: -L${SRCDIR}/ext/linux_x86_64 -lfliptengine -Wl,-rpath=${SRCDIR}/ext/linux_x86_64
+#cgo linux,amd64 LDFLAGS: -L${SRCDIR}/ext/linux_x86_64 -lfliptengine -Wl,-rpath,${SRCDIR}/ext/linux_x86_64
 #include <string.h>
 #include <stdlib.h>
 #include "flipt_engine.h"
