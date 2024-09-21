3847a6a772a9b19ff61374c9dbb90efdc6275ea9
Mark Phelps <209477+markphelps@users.noreply.github.com>
2024-09-20 21:35:46 -0400

    chore: more fmt
    Signed-off-by: Mark Phelps <209477+markphelps@users.noreply.github.com>

--- flipt-client-react/jest.config.js
+++ flipt-client-react/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-    reporters: ['default'],
-    moduleFileExtensions: ['ts', 'tsx', 'js'],
-    transform: {
-      '\\.(ts|tsx)$': 'ts-jest',
-    },
-    testRegex: '.*\\.test\\.(ts|tsx)$',
-    testPathIgnorePatterns: ['/node_modules/', '/examples/'],
-    testEnvironment: 'jest-environment-jsdom-global',
-  };
+  reporters: ['default'],
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
+  transform: {
+    '\\.(ts|tsx)$': 'ts-jest'
+  },
+  testRegex: '.*\\.test\\.(ts|tsx)$',
+  testPathIgnorePatterns: ['/node_modules/', '/examples/'],
+  testEnvironment: 'jest-environment-jsdom-global'
+};
