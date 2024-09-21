99a56b914b83bc6a1827f8c76690576f515bba22
Mark Phelps <209477+markphelps@users.noreply.github.com>
2024-09-21 15:55:48 -0400

    chore: fmt
    Signed-off-by: Mark Phelps <209477+markphelps@users.noreply.github.com>

--- flipt-client-react/rollup.config.mjs
+++ flipt-client-react/rollup.config.mjs
@@ -1,32 +1,28 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
-import dts from "rollup-plugin-dts";
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
 
 export default [
   {
-    input: "src/index.ts",
+    input: 'src/index.ts',
     output: [
       {
-        file: "dist/index.mjs",
-        format: "esm",
-        sourcemap: true,
+        file: 'dist/index.mjs',
+        format: 'esm',
+        sourcemap: true
       },
       {
-        file: "dist/index.cjs",
-        format: "cjs",
-        sourcemap: true,
-      },
+        file: 'dist/index.cjs',
+        format: 'cjs',
+        sourcemap: true
+      }
     ],
-    plugins: [
-      resolve(),
-      commonjs(),
-      typescript(),
-    ],
+    plugins: [resolve(), commonjs(), typescript()]
   },
   {
-    input: "dist/types/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "esm" }],
-    plugins: [dts()],
-  },
+    input: 'dist/types/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [dts()]
+  }
 ];
