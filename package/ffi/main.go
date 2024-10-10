e6d7ebb92b769fd9e18fb5a33b7fd206792d7c49
Mark Phelps <209477+markphelps@users.noreply.github.com>
2024-10-10 13:43:19 -0400

    chore: package win for other sdks
    Signed-off-by: Mark Phelps <209477+markphelps@users.noreply.github.com>

--- package/ffi/main.go
+++ package/ffi/main.go
@@ -149,7 +149,7 @@ var (
 		{id: "Linux-x86_64-musl", target: "x86_64-unknown-linux-musl", ext: "tar.gz", libc: musl},
 		{id: "Darwin-arm64", target: "aarch64-apple-darwin", ext: "tar.gz", libc: both},
 		{id: "Darwin-x86_64", target: "x86_64-apple-darwin", ext: "tar.gz", libc: both},
-		// {id: "Windows-x86_64", target: "x86_64-pc-windows-msvc", ext: "zip", libc: glibc},
+		{id: "Windows-x86_64", target: "x86_64-pc-windows-msvc", ext: "zip", libc: glibc},
 	}
 )
 
