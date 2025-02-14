20bc714672cd00802adddb711fd4006f242bbb60
Roman Dmytrenko <rdmytrenko@gmail.com>
2025-02-14 15:21:59 +0200

    drop support for mac x86_64
    Signed-off-by: Roman Dmytrenko <rdmytrenko@gmail.com>

--- flipt-engine-ffi/build.sh
+++ flipt-engine-ffi/build.sh
@@ -11,19 +11,17 @@ SDK=$1
 
 case $SDK in
 "swift")
-  rustup target add aarch64-apple-ios x86_64-apple-ios aarch64-apple-ios-sim x86_64-apple-darwin
+  rustup target add aarch64-apple-ios x86_64-apple-ios aarch64-apple-ios-sim
 
   cargo build -p flipt-engine-ffi --release --target=aarch64-apple-ios-sim
   cargo build -p flipt-engine-ffi --release --target=aarch64-apple-ios
   cargo build -p flipt-engine-ffi --release --target=aarch64-apple-darwin
-  cargo build -p flipt-engine-ffi --release --target=x86_64-apple-darwin
   rm -rf ../flipt-client-swift/Sources/FliptEngineFFI.xcframework
 
   xcodebuild -create-xcframework \
     -library ../target/aarch64-apple-ios-sim/release/libfliptengine.a -headers ./include/ \
     -library ../target/aarch64-apple-ios/release/libfliptengine.a -headers ./include/ \
     -library ../target/aarch64-apple-darwin/release/libfliptengine.a -headers ./include/ \
-    -library ../target/x86_64-apple-darwin/release/libfliptengine.a -headers ./include/ \
     -output "../flipt-client-swift/Sources/FliptEngineFFI.xcframework"
   ;;
 
