b89c7426c0c9036fcbc4e4bcfbaaec3afa63d520
Roman Dmytrenko <rdmytrenko@gmail.com>
2025-02-14 15:46:08 +0200

    build(ffi-engine): build ubuntu-22.04 (#718)

--- flipt-engine-ffi/build.sh
+++ flipt-engine-ffi/build.sh
@@ -16,14 +16,12 @@ case $SDK in
   cargo build -p flipt-engine-ffi --release --target=aarch64-apple-ios-sim
   cargo build -p flipt-engine-ffi --release --target=aarch64-apple-ios
   cargo build -p flipt-engine-ffi --release --target=aarch64-apple-darwin
-  cargo build -p flipt-engine-ffi --release --target=x86_64-apple-darwin
   rm -rf ../flipt-client-swift/Sources/FliptEngineFFI.xcframework
 
   xcodebuild -create-xcframework \
     -library ../target/aarch64-apple-ios-sim/release/libfliptengine.a -headers ./include/ \
     -library ../target/aarch64-apple-ios/release/libfliptengine.a -headers ./include/ \
     -library ../target/aarch64-apple-darwin/release/libfliptengine.a -headers ./include/ \
-    -library ../target/x86_64-apple-darwin/release/libfliptengine.a -headers ./include/ \
     -output "../flipt-client-swift/Sources/FliptEngineFFI.xcframework"
   ;;
 
