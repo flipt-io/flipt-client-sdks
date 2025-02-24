2a10df722f83dd96ec8fab7b87ff21c68a59bbd2
Mark Phelps <209477+markphelps@users.noreply.github.com>
2025-02-24 17:28:27 -0500

    chore: rm wasm-opt cause it  fails the build in CI
    Signed-off-by: Mark Phelps <209477+markphelps@users.noreply.github.com>

--- test/main.go
+++ test/main.go
@@ -310,8 +310,6 @@ func getWasmBuildContainer(_ context.Context, client *dagger.Client, hostDirecto
 	return client.Container(dagger.ContainerOpts{
 		Platform: dagger.Platform("linux/amd64"),
 	}).From("rust:1.83.0-bullseye").
-		WithExec(args("apt-get update")).
-		WithExec(args("apt-get install -y binaryen")).
 		WithWorkdir("/src").
 		WithDirectory("/src/flipt-engine-ffi", hostDirectory.Directory("flipt-engine-ffi")).
 		WithDirectory("/src/flipt-engine-wasm", hostDirectory.Directory("flipt-engine-wasm"), dagger.ContainerWithDirectoryOpts{
@@ -323,8 +321,7 @@ func getWasmBuildContainer(_ context.Context, client *dagger.Client, hostDirecto
 		WithDirectory("/src/flipt-evaluation", hostDirectory.Directory("flipt-evaluation")).
 		WithFile("/src/Cargo.toml", hostDirectory.File("Cargo.toml")).
 		WithExec(args("rustup target add " + target)).
-		WithExec(args("cargo build -p flipt-engine-wasm --release --target " + target)).                                                                        // Build the wasm module
-		WithExec(args("wasm-opt -O3 /src/target/wasm32-wasip1/release/flipt_engine_wasm.wasm -o /src/target/wasm32-wasip1/release/flipt_engine_wasm.opt.wasm")) // Optimize the wasm module
+		WithExec(args("cargo build -p flipt-engine-wasm --release --target " + target)) // Build the wasm module
 }
 
 // getWasmJSBuildContainer builds the wasm module for the Rust core for the client libraries to run
@@ -333,8 +330,6 @@ func getWasmJSBuildContainer(_ context.Context, client *dagger.Client, hostDirec
 	container := client.Container(dagger.ContainerOpts{
 		Platform: dagger.Platform("linux/amd64"),
 	}).From("rust:1.83.0-bullseye").
-		WithExec(args("apt-get update")).
-		WithExec(args("apt-get install -y binaryen")).
 		WithWorkdir("/src").
 		WithDirectory("/src/flipt-engine-ffi", hostDirectory.Directory("flipt-engine-ffi")).
 		WithDirectory("/src/flipt-engine-wasm", hostDirectory.Directory("flipt-engine-wasm"), dagger.ContainerWithDirectoryOpts{
