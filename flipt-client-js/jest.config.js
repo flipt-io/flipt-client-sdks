072113bdfc642a000803db81134f35ff45fe82a6
Mark Phelps <209477+markphelps@users.noreply.github.com>
2025-03-28 12:00:10 -0400

    chore: try to fix jest tests
    Signed-off-by: Mark Phelps <209477+markphelps@users.noreply.github.com>

--- flipt-client-js/jest.config.js
+++ flipt-client-js/jest.config.js
@@ -1,5 +1,19 @@
-module.exports = {
-  //testEnvironment: 'jsdom',
+/** @type {import('jest').Config} */
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
   setupFiles: ['<rootDir>/jest.polyfills.js'],
   moduleFileExtensions: ['ts', 'js', 'html']
 };
