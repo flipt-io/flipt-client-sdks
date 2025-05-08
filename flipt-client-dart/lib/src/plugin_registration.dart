d41f94051359b663b014ef307701d86042a15a9a
Mark Phelps <209477+markphelps@users.noreply.github.com>
2025-05-08 13:57:37 -0400

    chore: flutter
    Signed-off-by: Mark Phelps <209477+markphelps@users.noreply.github.com>

--- flipt-client-dart/lib/src/plugin_registration.dart
+++ flipt-client-dart/lib/src/plugin_registration.dart
@@ -1,7 +1,5 @@
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
-
 class FliptClient {
-  static void registerWith(Registrar registrar) {
-    // no-op for FFI plugins
+  static void registerWith(Object? _) {
+    // no-op for FFI
   }
 }
