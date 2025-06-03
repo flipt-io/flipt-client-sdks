982140fc855f88d1b550d3580ea8d10956b7d910
Roman Dmytrenko <rdmytrenko@gmail.com>
2025-06-03 14:54:25 +0300

    chore(js): migrate to eslint v9 configuration
    Signed-off-by: Roman Dmytrenko <rdmytrenko@gmail.com>

---
+++ flipt-client-js/eslint.config.cjs
@@ -0,0 +1,68 @@
+const {
+    defineConfig,
+    globalIgnores,
+} = require("eslint/config");
+
+const tsParser = require("@typescript-eslint/parser");
+const typescriptEslint = require("@typescript-eslint/eslint-plugin");
+const globals = require("globals");
+const js = require("@eslint/js");
+
+const {
+    FlatCompat,
+} = require("@eslint/eslintrc");
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+module.exports = defineConfig([{
+    languageOptions: {
+        parser: tsParser,
+        ecmaVersion: 2020,
+        sourceType: "module",
+
+        parserOptions: {
+            project: "./tsconfig.json",
+            tsconfigRootDir: __dirname,
+        },
+
+        globals: {
+            ...globals.node,
+            ...globals.browser,
+        },
+    },
+
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+    },
+
+    extends: compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended"),
+
+    rules: {
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+
+        "@typescript-eslint/no-unused-vars": ["error", {
+            argsIgnorePattern: "^_",
+        }],
+
+        "@typescript-eslint/no-unsafe-assignment": "off",
+        "@typescript-eslint/no-unsafe-member-access": "off",
+        "@typescript-eslint/no-unsafe-call": "off",
+        "@typescript-eslint/restrict-template-expressions": "off",
+        "@typescript-eslint/no-inferrable-types": "off",
+        "@typescript-eslint/ban-ts-comment": "off",
+
+        "@typescript-eslint/no-empty-object-type": "off",
+
+        "@typescript-eslint/no-empty-interface": "off",
+        "prefer-const": "error",
+
+        "no-console": ["error", {
+            allow: ["warn", "error"],
+        }],
+    },
+}, globalIgnores(["**/dist/", "**/node_modules/", "**/*.js", "**/*.mjs"])]);
