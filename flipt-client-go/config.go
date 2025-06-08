c4b464061d896731f6bd78543941fbafd91bff3a
Mark Phelps <209477+markphelps@users.noreply.github.com>
2025-06-08 11:40:12 -0400

    chore: doc; move options to config.go
    Signed-off-by: Mark Phelps <209477+markphelps@users.noreply.github.com>

--- flipt-client-go/config.go
+++ flipt-client-go/config.go
@@ -2,10 +2,119 @@ package flipt
 
 import (
 	"fmt"
+	"net"
 	"net/http"
 	"strings"
 	"time"
 )
+
+// Option configures the Flipt client via the provided config struct.
+type Option func(*config)
+
+// WithEnvironment sets the environment for the client.
+func WithEnvironment(environment string) Option {
+	return func(cfg *config) {
+		cfg.Environment = environment
+	}
+}
+
+// WithNamespace sets the namespace for the client.
+func WithNamespace(namespace string) Option {
+	return func(cfg *config) {
+		cfg.Namespace = namespace
+	}
+}
+
+// WithURL sets the URL for the client.
+func WithURL(url string) Option {
+	return func(cfg *config) {
+		cfg.URL = url
+	}
+}
+
+// WithHTTPClient sets the HTTP client for the client.
+// Note: if used in conjunction with WithRequestTimeout the request timeout on the HTTP client will be overridden.
+func WithHTTPClient(httpClient *http.Client) Option {
+	return func(cfg *config) {
+		cfg.HTTPClient = httpClient
+	}
+}
+
+// WithReference sets the reference for the client.
+func WithReference(ref string) Option {
+	return func(cfg *config) {
+		cfg.Reference = ref
+	}
+}
+
+// WithUpdateInterval sets the update interval for the client.
+func WithUpdateInterval(updateInterval time.Duration) Option {
+	return func(cfg *config) {
+		cfg.UpdateInterval = updateInterval
+	}
+}
+
+// WithClientTokenAuthentication sets client token authentication.
+func WithClientTokenAuthentication(token string) Option {
+	return func(cfg *config) {
+		cfg.Authentication = clientTokenAuthentication{Token: token}
+	}
+}
+
+// WithJWTAuthentication sets JWT authentication.
+func WithJWTAuthentication(token string) Option {
+	return func(cfg *config) {
+		cfg.Authentication = jwtAuthentication{Token: token}
+	}
+}
+
+// WithFetchMode sets the fetch mode for the client.
+func WithFetchMode(fetchMode FetchMode) Option {
+	return func(cfg *config) {
+		cfg.FetchMode = fetchMode
+	}
+}
+
+// WithErrorStrategy sets the error strategy for the client.
+func WithErrorStrategy(errorStrategy ErrorStrategy) Option {
+	return func(cfg *config) {
+		cfg.ErrorStrategy = errorStrategy
+	}
+}
+
+// WithRequestTimeout sets the request timeout for the client.
+// Note: this is only used for polling mode.
+// Note: setting this will override the HTTP client timeout.
+func WithRequestTimeout(timeout time.Duration) Option {
+	return func(cfg *config) {
+		cfg.RequestTimeout = timeout
+	}
+}
+
+// defaultHTTPClient is the default HTTP client used by the client.
+// It is used to make requests to the upstream Flipt instance and is configured to be best compatible with streaming mode.
+var defaultHTTPClient = &http.Client{
+	Transport: &http.Transport{
+		DialContext: (&net.Dialer{
+			Timeout:   10 * time.Second,
+			KeepAlive: 5 * time.Second, // More aggressive TCP keepalive
+		}).DialContext,
+		IdleConnTimeout:       60 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ResponseHeaderTimeout: 10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+		ForceAttemptHTTP2:     true,
+		MaxIdleConns:          100,
+		MaxIdleConnsPerHost:   10,
+		// Enable HTTP/2 ping frames and other settings
+		Proxy:           http.ProxyFromEnvironment,
+		ReadBufferSize:  32 * 1024,
+		WriteBufferSize: 32 * 1024,
+		// HTTP/2 specific settings
+		MaxResponseHeaderBytes: 4 * 1024, // 4KB
+		DisableCompression:     false,    // Enable compression
+	},
+}
 
 // config contains all configuration options for the Flipt client.
 type config struct {
@@ -15,7 +124,7 @@ type config struct {
 	Namespace string
 	// URL of the upstream Flipt instance. Defaults to "http://localhost:8080".
 	URL string
-	// RequestTimeout is the timeout for requests to the upstream Flipt instance. Defaults to 30s.
+	// RequestTimeout is the timeout for requests to the upstream Flipt instance. Defaults to no timeout.
 	RequestTimeout time.Duration
 	// UpdateInterval is how often to fetch new flag state. Defaults to 120s.
 	UpdateInterval time.Duration
