fb068dcc148ec6017bcdeebd8e3c1de44f010155
Mark Phelps <209477+markphelps@users.noreply.github.com>
2025-06-08 18:48:17 -0400

    chore: Update swift android test workflows (#1066)
    * chore: fix android/swift test workflows
    
    Signed-off-by: Mark Phelps <209477+markphelps@users.noreply.github.com>
    
    * chore: fmt with prettier
    
    Signed-off-by: Mark Phelps <209477+markphelps@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Mark Phelps <209477+markphelps@users.noreply.github.com>

--- flipt-client-js/eslint.config.cjs
+++ flipt-client-js/eslint.config.cjs
@@ -1,68 +1,75 @@
-const {
-    defineConfig,
-    globalIgnores,
-} = require("eslint/config");
+const { defineConfig, globalIgnores } = require('eslint/config');
 
-const tsParser = require("@typescript-eslint/parser");
-const typescriptEslint = require("@typescript-eslint/eslint-plugin");
-const globals = require("globals");
-const js = require("@eslint/js");
+const tsParser = require('@typescript-eslint/parser');
+const typescriptEslint = require('@typescript-eslint/eslint-plugin');
+const globals = require('globals');
+const js = require('@eslint/js');
 
-const {
-    FlatCompat,
-} = require("@eslint/eslintrc");
+const { FlatCompat } = require('@eslint/eslintrc');
 
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
 });
 
-module.exports = defineConfig([{
+module.exports = defineConfig([
+  {
     languageOptions: {
-        parser: tsParser,
-        ecmaVersion: 2020,
-        sourceType: "module",
+      parser: tsParser,
+      ecmaVersion: 2020,
+      sourceType: 'module',
 
-        parserOptions: {
-            project: "./tsconfig.json",
-            tsconfigRootDir: __dirname,
-        },
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname
+      },
 
-        globals: {
-            ...globals.node,
-            ...globals.browser,
-        },
+      globals: {
+        ...globals.node,
+        ...globals.browser
+      }
     },
 
     plugins: {
-        "@typescript-eslint": typescriptEslint,
+      '@typescript-eslint': typescriptEslint
     },
 
-    extends: compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended"),
+    extends: compat.extends(
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended'
+    ),
 
     rules: {
-        "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/explicit-module-boundary-types": "off",
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
 
-        "@typescript-eslint/no-unused-vars": ["error", {
-            argsIgnorePattern: "^_",
-        }],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_'
+        }
+      ],
 
-        "@typescript-eslint/no-unsafe-assignment": "off",
-        "@typescript-eslint/no-unsafe-member-access": "off",
-        "@typescript-eslint/no-unsafe-call": "off",
-        "@typescript-eslint/restrict-template-expressions": "off",
-        "@typescript-eslint/no-inferrable-types": "off",
-        "@typescript-eslint/ban-ts-comment": "off",
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/restrict-template-expressions': 'off',
+      '@typescript-eslint/no-inferrable-types': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off',
 
-        "@typescript-eslint/no-empty-object-type": "off",
+      '@typescript-eslint/no-empty-object-type': 'off',
 
-        "@typescript-eslint/no-empty-interface": "off",
-        "prefer-const": "error",
+      '@typescript-eslint/no-empty-interface': 'off',
+      'prefer-const': 'error',
 
-        "no-console": ["error", {
-            allow: ["warn", "error"],
-        }],
-    },
-}, globalIgnores(["**/dist/", "**/node_modules/", "**/*.js", "**/*.mjs"])]);
+      'no-console': [
+        'error',
+        {
+          allow: ['warn', 'error']
+        }
+      ]
+    }
+  },
+  globalIgnores(['**/dist/', '**/node_modules/', '**/*.js', '**/*.mjs'])
+]);
